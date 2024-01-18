7618d0e0fe8a220b4080808bdefc9699881b338a
Kulpot <kulpotwahu@gmail.com>
2024-01-18 15:01:28 +0800

    ...

--- C++ Header Files/CppScratchPad.cpp
+++ C++ Header Files/CppScratchPad.cpp
@@ -5,3 +5,9 @@ using std::endl;
 // -------------------- C++ Header Files -------------------------------------------------------
 //ref link:https://www.youtube.com/watch?v=Wa6irrxFG88&list=PLRwVmtr-pp05Bl0j6lwXd8tU754nUEB5P
 
+void foo();
+
+void main()
+{
+	foo();
+}
