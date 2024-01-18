a7c3312586d02e4b5805dfbf7b244190e1269034
Kulpot <kulpotwahu@gmail.com>
2024-01-18 15:01:09 +0800

    ...

---
+++ C++ Header Files/CppScratchPad.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// -------------------- C++ Header Files -------------------------------------------------------
+//ref link:https://www.youtube.com/watch?v=Wa6irrxFG88&list=PLRwVmtr-pp05Bl0j6lwXd8tU754nUEB5P
+
